bead87623132757df2dcf66d574db7703dcae036
Evgeniy <valzubof@yandex.ru>
2023-03-04 16:16:03 +0400

    add model Deposit

--- deposit_calc/calculator/models.py
+++ deposit_calc/calculator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Deposit(models.Model):
+    date = models.DateField(auto_now_add=True)
+    periods = models.IntegerField()
+    amount = models.IntegerField()
+    rate = models.FloatField()
+
+    def __str__(self):
+        return self.name
